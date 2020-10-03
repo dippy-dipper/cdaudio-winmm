f85627f05dbb7a1398666a8cd5cca897eb0fe426
dippy-dipper <70149011+dippy-dipper@users.noreply.github.com>
2020-10-04 02:59:48 +0300

    Update make.cmd

--- cdaudioplr_src/make.cmd
+++ cdaudioplr_src/make.cmd
@@ -1,3 +1,3 @@
 windres cdaudioplr.rc -O coff -o cdaudioplr.res
-gcc cdaudioplr.c cdaudioplr.res -lwinmm -D_DEBUG -mwindows -o cdaudioplr.exe
+g++ cdaudioplr.c cdaudioplr.res -lwinmm -D_DEBUG -mwindows -o cdaudioplr.exe
 pause
